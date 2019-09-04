5c59815feca3a714a5f20c98beed24770f7bf6b6
DARVIN <DARVIN@LAPTOP-JV6B3PDN>
2019-09-04 11:35:10 -0500

    modifique la ayuda de manera especial

--- views/ayuda/index.php
+++ views/ayuda/index.php
@@ -10,6 +10,8 @@
             <h1 class="center">seccion de ayuda</h1>
             <p>somos peru en todas partes </p>
             <p> todos somos dignos</p>
+            <p>gracias a Dios ya lo hicimos nos ha demorado pero ya esta</p>
+            <p>lo sabia jaaaa...</p>
         </div>
         <?php require 'views/footer.php'; ?>
 
